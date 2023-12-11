a743565ffc9f4a8908064d74fd5c169ac0c9a122
nftchance <chance@utc24.io>
2023-12-11 00:07:23 -0600

    chore: all mock tests to forge

--- src/contracts/sockets/Plug.Mock.Socket.t.sol
+++ src/contracts/sockets/Plug.Mock.Socket.t.sol
@@ -17,6 +17,11 @@ contract PlugMockSocketTest is PRBTest, StdCheats, TestPlus {
     address internal signer;
     uint256 internal signerPrivateKey;
 
+    uint8 internal v;
+    bytes32 internal r;
+    bytes32 internal s;
+    bytes32 internal digest;
+
     function setUp() public {
 		mock = new PlugMockSocket('PlugMockSocket', '0.0.0');
 
@@ -37,12 +42,12 @@ contract PlugMockSocketTest is PRBTest, StdCheats, TestPlus {
 		mock.emptyEcho();
     }
 
-    function testFail_MutedEcho() public { 
+    function testFail_MutedEcho() public {
 		vm.expectRevert('EchoMuted');
 		mock.mutedEcho();
     }
 
-    function test_GetLivePinSigner() public { 
+    function test_GetLivePlugsSigner() public {
 	    PlugTypesLib.Fuse[] memory fuses = new PlugTypesLib.Fuse[](0);
 		PlugTypesLib.Pin memory pin = PlugTypesLib.Pin({
 			neutral: signer,
@@ -51,14 +56,125 @@ contract PlugMockSocketTest is PRBTest, StdCheats, TestPlus {
 			salt: bytes32(0),
 			forced: true
 		});
-		bytes32 digest = mock.getPinDigest(pin);
-		(uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, digest);
-		bytes memory signature = abi.encodePacked(r, s, v);
+
+		digest = mock.getPinDigest(pin);
+		(v, r, s) = vm.sign(signerPrivateKey, digest);
+
+		bytes memory pinSignature = abi.encodePacked(r, s, v);
 		PlugTypesLib.LivePin memory livePin = PlugTypesLib.LivePin({
 			pin: pin,
-			signature: signature
+			signature: pinSignature
 		});
 		address pinSigner = mock.getLivePinSigner(livePin);
 		assertEq(pinSigner, signer);
+
+        bytes memory encodedTransaction = abi.encodeWithSelector(
+            mock.mutedEcho.selector
+        );
+        PlugTypesLib.Current memory current = PlugTypesLib.Current({
+            ground: address(mock),
+            voltage: 0,
+            data: encodedTransaction
+        });
+        PlugTypesLib.LivePin[] memory livePins = new PlugTypesLib.LivePin[](1);
+        livePins[0] = livePin;
+        PlugTypesLib.Plug memory Plug = PlugTypesLib.Plug({
+            pins: livePins,
+            current: current,
+            forced: true
+        });
+        PlugTypesLib.Plug[] memory plugsArray = new PlugTypesLib.Plug[](1);
+        plugsArray[0] = Plug;
+        PlugTypesLib.Plugs memory plugs = PlugTypesLib.Plugs({
+            breaker: PlugTypesLib.Breaker({
+                nonce: 1,
+                queue: 0
+            }),
+            plugs: plugsArray
+        });
+        digest = mock.getPlugsDigest(plugs);
+        (v, r, s) = vm.sign(signerPrivateKey, digest);
+        bytes memory plugsSignature = abi.encodePacked(r, s, v);
+        PlugTypesLib.LivePlugs memory livePlugs = PlugTypesLib.LivePlugs({
+            plugs: plugs,
+            signature: plugsSignature
+        });
+        address plugsSigner = mock.getLivePlugsSigner(livePlugs);
+        assertEq(plugsSigner, signer);
+    }
+
+    function test_PlugEmptyEcho() public {
+        bytes memory encodedTransaction = abi.encodeWithSelector(
+            mock.emptyEcho.selector
+        );
+        PlugTypesLib.Current memory current = PlugTypesLib.Current({
+            ground: address(mock),
+            voltage: 21000,
+            data: encodedTransaction
+        });
+        PlugTypesLib.Plug memory Plug = PlugTypesLib.Plug({
+            pins: new PlugTypesLib.LivePin[](0),
+            current: current,
+            forced: true
+        });
+        PlugTypesLib.Plug[] memory plugsArray = new PlugTypesLib.Plug[](1);
+        plugsArray[0] = Plug;
+        PlugTypesLib.Plugs memory plugs = PlugTypesLib.Plugs({
+            breaker: PlugTypesLib.Breaker({
+                nonce: 1,
+                queue: 0
+            }),
+            plugs: plugsArray
+        });
+        digest = mock.getPlugsDigest(plugs);
+        (v, r, s) = vm.sign(signerPrivateKey, digest);
+        bytes memory plugsSignature = abi.encodePacked(r, s, v);
+        PlugTypesLib.LivePlugs memory livePlugs = PlugTypesLib.LivePlugs({
+            plugs: plugs,
+            signature: plugsSignature
+        });
+        address plugsSigner = mock.getLivePlugsSigner(livePlugs);
+        assertEq(plugsSigner, signer);
+        PlugTypesLib.LivePlugs[] memory livePlugsArray = new PlugTypesLib.LivePlugs[](1);
+        livePlugsArray[0] = livePlugs;
+        mock.plug(livePlugsArray);
+    }
+
+
+    function testFail_PlugMutedEcho() public {
+        bytes memory encodedTransaction = abi.encodeWithSelector(
+            mock.mutedEcho.selector
+        );
+        PlugTypesLib.Current memory current = PlugTypesLib.Current({
+            ground: address(mock),
+            voltage: 21000,
+            data: encodedTransaction
+        });
+        PlugTypesLib.Plug memory Plug = PlugTypesLib.Plug({
+            pins: new PlugTypesLib.LivePin[](0),
+            current: current,
+            forced: true
+        });
+        PlugTypesLib.Plug[] memory plugsArray = new PlugTypesLib.Plug[](1);
+        plugsArray[0] = Plug;
+        PlugTypesLib.Plugs memory plugs = PlugTypesLib.Plugs({
+            breaker: PlugTypesLib.Breaker({
+                nonce: 1,
+                queue: 0
+            }),
+            plugs: plugsArray
+        });
+        digest = mock.getPlugsDigest(plugs);
+        (v, r, s) = vm.sign(signerPrivateKey, digest);
+        bytes memory plugsSignature = abi.encodePacked(r, s, v);
+        PlugTypesLib.LivePlugs memory livePlugs = PlugTypesLib.LivePlugs({
+            plugs: plugs,
+            signature: plugsSignature
+        });
+        address plugsSigner = mock.getLivePlugsSigner(livePlugs);
+        assertEq(plugsSigner, signer);
+        PlugTypesLib.LivePlugs[] memory livePlugsArray = new PlugTypesLib.LivePlugs[](1);
+        livePlugsArray[0] = livePlugs;
+        mock.plug(livePlugsArray);
     }
 }
