a6b6288b0c462835e45a58ad494efdc7f3571ef9
nftchance <chance@utc24.io>
2024-03-22 03:45:43 +0000

    chore: update build artifacts

--- src/contracts/base/Plug.Factory.t.sol
+++ src/contracts/base/Plug.Factory.t.sol
@@ -11,7 +11,9 @@ import {
 } from "../abstracts/test/Plug.Test.sol";
 
 contract PlugFactoryTest is Test {
-    event Transfer(address indexed from, address indexed to, uint256 indexed id);
+    event Transfer(
+        address indexed from, address indexed to, uint256 indexed id
+    );
 
     function setUp() public virtual {
         setUpPlug();
@@ -20,10 +22,10 @@ contract PlugFactoryTest is Test {
     function test_DeployDeterministic(uint256) public {
         vm.deal(address(this), 1000 ether);
         uint256 initialValue = _random() % 100 ether;
-        bytes32 salt =
-            bytes32(abi.encodePacked(address(1), uint96(0)));
+        bytes32 salt = bytes32(abi.encodePacked(address(1), uint96(0)));
         address implementation = factory.implementations(0);
-        uint256 tokenId = uint256(uint160(factory.getAddress(implementation, salt)));
+        uint256 tokenId =
+            uint256(uint160(factory.getAddress(implementation, salt)));
 
         vm.expectEmit(address(factory));
         emit Transfer(address(0), address(1), tokenId);
